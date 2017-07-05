6e013368739678feb4bee5cdd5c395cc91c2d955
Paul Eden <paul.eden@byu.edu>
2017-07-05 00:40:53 +0000

    moved to use handel

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -xe
-
-#pip install -r requirements.txt -t .
-aws cloudformation package --template-file sam_pre.yml --output-template-file sam_post.yml --s3-bucket byu-us-east-1-fhe-assignment-alexa-skill --profile devstg-us-east-1
-aws cloudformation deploy --template-file sam_post.yml --stack-name fhe-alexa-skill --capabilities CAPABILITY_IAM --profile devstg-us-east-1

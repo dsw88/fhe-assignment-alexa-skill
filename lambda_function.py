29ad19e181d974f1346dfd90791f5938e46a4094
Paul Eden <paul.eden@byu.edu>
2017-09-06 02:46:53 +0000

    added debugging on setupintent and updated dialog config

--- lambda_function.py
+++ lambda_function.py
@@ -299,6 +299,7 @@ def help_intent_handler(request):
     return respond("Hi there! I can tell you and your family which family members have which assignments for family home evening each week. And, I'll automatically rotate those assignments each week so you don't have to do that. To start, just say, 'Alexa, open family home evening assignments.'")
 
 def setup_intent_handler(request):
+    print(request)
     family_member = get_slot(request, 'FamilyMember')
     assignment = get_slot(request, 'Assignment')
 
